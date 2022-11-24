afd44d3968a470139f3d740008b9baf86adcd12d
albert <1132265836@qq.com>
2022-11-24 11:58:28 +0800

    hot-fix 第三次提交

--- src/Hello.java
+++ src/Hello.java
@@ -2,8 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
+        System.out.println("hello-hot-fix修补bug");
+        System.out.println("hello-hot-fix修补bug");
         System.out.println("hello--hot-fix修改");
         System.out.println("hot-fix");
         System.out.println("hot-fix");
